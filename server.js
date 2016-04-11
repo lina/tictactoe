7e096172480023a1f9fd5f160505a6074b4a560d
Yaqing LU <Lina@Yaqings-MacBook-Pro.local>
2016-04-11 00:27:55 -0700

    [deployVersion] disabled hot reloading etc

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ var port = 5000;
 
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 
-// Allow CORS
-app.use('/', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// // Allow CORS
+// app.use('/', function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', "*");
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 // Customized links for dev vs prod environment
 // var challengesSrcBundleLink, reactLibraryLink, reactDomLibraryLink;
@@ -31,26 +31,26 @@ app.get('/', function(req, res){
   res.render('pages/index');
 });
 
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var webpackConfig = require('./webpack.config.js');
-var webserverPort = 3000;
+// var webpack = require('webpack');
+// var WebpackDevServer = require('webpack-dev-server');
+// var webpackConfig = require('./webpack.config.js');
+// var webserverPort = 3000;
 
-new WebpackDevServer(webpack(webpackConfig), {
-  publicPath: webpackConfig.output.publicPath,
-  hot:true,
-  inline: true,
-  historyApiFallback: true,
-  stats: {
-    colors: true
-  }
-}).listen(webserverPort, '0.0.0.0', function(err, result) {
-  if(err) {
-    console.log({err: err}, 'Error when connecting to WebpackDevServer');
-  }
+// new WebpackDevServer(webpack(webpackConfig), {
+//   publicPath: webpackConfig.output.publicPath,
+//   hot:true,
+//   inline: true,
+//   historyApiFallback: true,
+//   stats: {
+//     colors: true
+//   }
+// }).listen(webserverPort, '0.0.0.0', function(err, result) {
+//   if(err) {
+//     console.log({err: err}, 'Error when connecting to WebpackDevServer');
+//   }
 
-  console.log({port: webserverPort}, 'Listening on WebpackDevServer port');
-});
+//   console.log({port: webserverPort}, 'Listening on WebpackDevServer port');
+// });
 
 // specify static content folder
 app.use('/', express.static(__dirname + '/client'));
